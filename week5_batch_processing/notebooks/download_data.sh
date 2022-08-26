0844ded5d92a63d7fd745c31b236021823134a2c
Shemoxcellency <oladeleoluwaseun8@gmail.com>
2022-08-26 02:50:31 +0000

    spark files for commit

---
+++ week5_batch_processing/notebooks/download_data.sh
@@ -0,0 +1,24 @@
+set -e  #stop execution if job encounters error at any step
+#https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet
+TAXI_TYPE=$1 # "yellow, green"
+YEAR=$2 # 2020, 2021
+
+URL_PREFIX="https://d37ci6vzurychx.cloudfront.net/trip-data"
+
+for MONTH in {1..12}; do
+  FMONTH=`printf "%02d" ${MONTH}`
+
+  URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.parquet"
+
+  LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
+  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.parquet"
+  LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+
+  echo "donwloading ${URL} to ${LOCAL_PATH}"
+  mkdir -p ${LOCAL_PREFIX}
+  wget ${URL} -O ${LOCAL_PATH}
+
+  echo "compressing ${LOCAL_PATH}"
+  gzip ${LOCAL_PATH}
+done
+
